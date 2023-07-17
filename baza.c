5cbb7b49083dc38942948155a449cc804479644a
L1pku <138522987+L1pku@users.noreply.github.com>
2023-07-17 19:46:34 +0300

    changes 17.07.c

--- baza.c
+++ baza.c
@@ -50,4 +50,49 @@ int main() {
         createApartment(4, 1, 17000.0),
         createApartment(2, 3, 13000.0),
         createApartment(3, 5, 15000.0)
+    };
+    int numOfLvivApartments = sizeof(lvivApartments) / sizeof(lvivApartments[0]);
+
+    Apartment odessaApartments[] = {
+        createApartment(1, 1, 12000.0),
+        createApartment(2, 1, 14500.0),
+        createApartment(3, 1, 1800.0),
+        createApartment(1, 2, 13000.0),
+        createApartment(2, 2, 15000.0),
+        createApartment(3, 2, 19000.0),
+        createApartment(1, 3, 13800.0),
+        createApartment(2, 3, 16000.0),
+        createApartment(3, 3, 19000.0),
+        createApartment(4, 1, 17000.0),
+        createApartment(2, 3, 13000.0),
+        createApartment(3, 5, 15000.0),
+        createApartment(3, 4, 18000.0),
+        createApartment(2, 4, 15000.0),
+        createApartment(3, 9, 21000.0)
+    };
+    int numOfOdessaApartments = sizeof(odessaApartments) / sizeof(odessaApartments[0]);
+
+    // Вибірка квартир за критеріями
+    int desiredRoomCount;
+    int desiredFloor;
+    float desiredRentPrice;
+
+    printf("Введіть бажану кількість кімнат: ");
+    scanf("%d", &desiredRoomCount);
+
+    printf("Введіть бажаний поверх: ");
+    scanf("%d", &desiredFloor);
+
+    printf("Введіть максимальну ціну оренди: ");
+    scanf("%f", &desiredRentPrice);
+
+    // Пошук та виведення квартир, що задовольняють вказані критерії
+    printf("Результати пошуку:\n");
+    printf("Квартири в Києві:\n");
+    for (int i = 0; i < numOfKievApartments; i++) {
+        if (kievApartments[i].roomCount == desiredRoomCount &&
+            kievApartments[i].floor == desiredFloor &&
+            kievApartments[i].rentPrice <= desiredRentPrice) {
+            printApartment(kievApartments[i]);
+        }
     }
